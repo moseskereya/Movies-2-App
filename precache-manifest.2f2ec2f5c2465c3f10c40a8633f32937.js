eb984757e1c5fb87942dee82066836a062ace043
MOSES <MOSESLAZARO2019@outlook.com>
2019-11-30 17:17:27 +0300

    Updates

---
+++ precache-manifest.2f2ec2f5c2465c3f10c40a8633f32937.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d854f26afff38375ac8e0387957516",
+    "url": "/Movies-2-App/index.html"
+  },
+  {
+    "revision": "0aebd579ad08bb3f661f",
+    "url": "/Movies-2-App/static/css/main.8f86ce91.chunk.css"
+  },
+  {
+    "revision": "5e8df04cd811ba22a804",
+    "url": "/Movies-2-App/static/js/2.e70950e6.chunk.js"
+  },
+  {
+    "revision": "0aebd579ad08bb3f661f",
+    "url": "/Movies-2-App/static/js/main.a69216ed.chunk.js"
+  },
+  {
+    "revision": "1da48b689b9ed1776c2e",
+    "url": "/Movies-2-App/static/js/runtime-main.d3691707.js"
+  }
+]);
